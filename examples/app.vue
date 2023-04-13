abf66a1d88614c06b0137bdf9b0eb8b43123949f
nikai <shopal-test@zhangjiarong.local>
2023-04-13 12:14:21 +0800

    部分日期选择英文显示

--- examples/app.vue
+++ examples/app.vue
@@ -1,38 +1,38 @@
 <template>
   <div class="app">
     <h1>K-Form</h1>
-    <Space direction="vertical">
-      <Card title="静态表单" size="small">
-        <k-form-list ref="form1" :form-list="normal" @change="formChange" @submit="handleSubmit('form1')" />
-      </Card>
-      <Card title="动态表单" size="small">
-        <k-form-list ref="form2" :form-list="dynamic" @change="formChange" @submit="handleSubmit('form2')" />
-      </Card>
-      <Card title="弹窗抽屉表单" size="small">
-        <Space>
-          <Button type="primary" @click="showModalForm">弹窗</Button>
-          <Button type="primary" @click="showDrawerForm">抽屉</Button>
-        </Space>
-      </Card>
-      <Card title="虚拟列表下拉选择" size="small">
-        <Row style="padding-top: 20px">
-          <Col :span="3" style="text-align: right;line-height: 32px;">单独使用：</Col>
-          <Col :span="21">
-            <k-select style="width: 180px" :options="options" placeholder="请选择"/>
-          </Col>
-        </Row>
-        <k-form-list ref="form3" :form-list="virtual" @change="formChange" @submit="handleSubmit('form3')" />
-      </Card>
-      <Card title="表单设计器" size="small">
-        <k-editor/>
-      </Card>
-      <div></div>
-    </Space>
-    <!-- 弹窗 -->
     <ConfigProvider :locale="locale">
+      <Space direction="vertical">
+        <Card title="静态表单" size="small">
+          <k-form-list ref="form1" :form-list="normal" @change="formChange" @submit="handleSubmit('form1')" />
+        </Card>
+        <Card title="动态表单" size="small">
+          <k-form-list ref="form2" :form-list="dynamic" @change="formChange" @submit="handleSubmit('form2')" />
+        </Card>
+        <Card title="弹窗抽屉表单" size="small">
+          <Space>
+            <Button type="primary" @click="showModalForm">弹窗</Button>
+            <Button type="primary" @click="showDrawerForm">抽屉</Button>
+          </Space>
+        </Card>
+        <Card title="虚拟列表下拉选择" size="small">
+          <Row style="padding-top: 20px">
+            <Col :span="3" style="text-align: right;line-height: 32px;">单独使用：</Col>
+            <Col :span="21">
+              <k-select style="width: 180px" :options="options" placeholder="请选择"/>
+            </Col>
+          </Row>
+          <k-form-list ref="form3" :form-list="virtual" @change="formChange" @submit="handleSubmit('form3')" />
+        </Card>
+        <Card title="表单设计器" size="small">
+            <k-editor/>
+        </Card>
+        <div></div>
+      </Space>
+      <!-- 弹窗 -->
       <k-form-modal title="弹窗表单" width="500px" :visible.sync="mVisible" :form-list="dynamic" @submit="handleMSubmit" />
+      <k-form-drawer title="抽屉表单" width="500px" :visible.sync="dVisible" :form-list="dynamic" @submit="handleMSubmit" />
     </ConfigProvider>
-    <k-form-drawer title="抽屉表单" width="500px" :visible.sync="dVisible" :form-list="dynamic" @submit="handleMSubmit" />
   </div>
 </template>
 
